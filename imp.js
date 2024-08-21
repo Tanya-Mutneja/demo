5ddd06238f3b9043ae9d3205e644057ead5ca3bd
Tanya-Mutneja <tanyamutneja22@gmail.com>
2024-08-21 17:48:24 +0530

    first commit

---
+++ imp.js
@@ -0,0 +1,15 @@
+// javascript is single thread ,sync language 
+//engine will not wait for anything 
+// await keyword always used inside async function 
+// async function always return 
+
+
+// simple function 
+function dataleaao(){
+    return 'Data returned by simple function ';
+}
+console.log(dataleaao());
+
+// async function
+async function dataleaao2()
+
